327dc7f48fea12101cea810d8bf66230275e0d8f
unknown <jasmineakbari@yahoo.com>
2021-01-13 19:04:15 -0500

    edited port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
 
+const PORT = process.env.PORT || 3001;
+
 // Initiates server
 const app = express();
 
@@ -52,6 +54,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 // enables server to listen for requests
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
