74472964cac95947d5332f02d69a1392f01fad6a
unknown <jasmineakbari@yahoo.com>
2021-01-15 18:59:21 -0500

    added user input handling functionality and ability add new animals to server list

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
-const { animals } = require('./data/animals.json');
+const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 
 // Initiates server
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -34,7 +42,7 @@ function filterByQuery(query, animalsArray) {
         });
     }
     if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === querty.diet);
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
     if (query.species) {
         filteredResults = filteredResults.filter(animal => animal.species === query.species);
@@ -50,7 +58,34 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animals.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+}
 
+// function to validate new animals added to array
+function validateAnimal(animal) {
+    if(!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
+// request after PORT received
 // two arguments 1st is route client will 'request' from 2nd is using send from 'response'parameter
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -68,6 +103,21 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
 
+});
+
+// this handles client side user input
+app.post('/api/animals', (req, res) => {
+
+    // set it based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
 });
 
 // enables server to listen for requests
