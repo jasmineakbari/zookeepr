45e09ae48f56f189d27279a4163f1b93d51541ec
unknown <jasmineakbari@yahoo.com>
2021-01-16 12:29:43 -0500

    completed refactor and added zookeeper page functionality

---
+++ routes/apiRoutes/animalRoutes.js
@@ -0,0 +1,40 @@
+const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { animals } = require('../../data/animals');
+const router = require('express').Router();
+
+// request after PORT received
+// two arguments 1st is route client will 'request' from 2nd is using send from 'response'parameter
+router.get('/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+router.get('/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+
+});
+
+// this handles client side user input
+router.post('/animals', (req, res) => {
+
+    // set it based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const express = require('express');
 const { animals } = require('./data/animals');
@@ -11,131 +13,16 @@ const app = express();
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+
 //parse incoming JSON data
 app.use(express.json());
+
+// adds api routes anytimes navigating to localhost/api and routes to html if '/'
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 // gives access to all assets i.e. css and js pages in public dir
 app.use(express.static('public'));
-
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-
-    // saving animalsArray as filtered results
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        //save personalityTraits as a dedicated Array
-        //if persoalityTraits is a string, place into a new array and save
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        } else {
-            personalityTraitsArray = query.personalityTraits;
-        }
-        // Loop through each trait in the personalityTraits Array
-        personalityTraitsArray.forEach(trait => {
-            // Check the trait against each animal in the filteredResults Array
-            // Remember, it is initially a copy of the animalsArray
-            // but here we're updating for each trait in the .forEach() loop
-            // For each trait being targeted by the filter, the filteredResults
-            // array will then contain only the entries that contain the trait,
-            // so at the end we'll have an array of animals that have every one
-            // of the traits when the .forEach() loop is finished.
-            filteredResults = filteredResults.filter(
-                animal => animal.personalityTraits.indexOf(trait) !== -1
-            );
-        });
-    }
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
-}
-
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id) [0];
-    return result;
-}
-
-function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animals.push(animal);
-    fs.writeFileSync(
-        path.join(__dirname, './data/animals.json'),
-        JSON.stringify({ animals: animalsArray }, null, 2)
-    );
-    return animal;
-}
-
-// function to validate new animals added to array
-function validateAnimal(animal) {
-    if(!animal.name || typeof animal.name !== 'string') {
-        return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-        return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-        return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false;
-    }
-    return true;
-}
-
-// request after PORT received
-// two arguments 1st is route client will 'request' from 2nd is using send from 'response'parameter
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-
-});
-
-// this handles client side user input
-app.post('/api/animals', (req, res) => {
-
-    // set it based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-
-    // if any data in req.body is incorrect, send 400 error back
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-    } else {
-        const animal = createNewAnimal(req.body, animals);
-        res.json(animal);
-    }
-});
-
-// route for index.html this is also the main page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-// route for animals.html showing list of all animals including newly added
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-// route for zookeepers.html page
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-})
 
 // enables server to listen for requests should always be at the very bottom of js file
 app.listen(PORT, () => {
